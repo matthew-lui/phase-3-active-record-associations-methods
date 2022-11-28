719e9b3037844516bdd5454b01d55a2acc968adf
matthew-lui <107003143+matthew-lui@users.noreply.github.com>
2022-11-28 14:42:03 -0500

    commit tree 87189054e760a54fb60b4853067d1a12b8bf4221

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,20 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artists.count
+    # second variable is the array and then count. 
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    # self.artists.map do |artist|
+    #   artist.name 
+    self.artists.pluck(:name)
+    # the pluck method will just go into the array and pluck/pull out the name from our array. 
   end
 end
